519d7b012fc33e7ed0f874cf756c75a9e6250ded
carolinafrederico <iam@carolinafrederico.com>
2025-03-23 23:12:15 -0400

    Exercise 2

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
+// const adventurer = {
+//     name: "Robin",
+//     health: 10,
+//     inventory: ["sword", "potion", "artifact"]
+//     }
+//     //Using for... of loop
+//     for (let item of adventurer.inventory) {
+//         console.log(item);
+//     }
+//     const adventurer = {
+// name: "Robin",
+// health: 10,
+// inventory: ["sword", "potion", "artifact"],
+// companion: {
+//     name: "Leo",
+//     type: "Cat"
+
 const adventurer = {
     name: "Robin",
     health: 10,
-    inventory: ["sword", "potion", "artifact"]
+    inventory: ["sword", "potion", "artifact"],
+    companion: {
+        name: "Leo",
+        type: "Cat",
+        companion: {
+            name: "Frank",
+            type: "Flea",
+            belongings: ["small hat", "sunglasses"]
+        }
     }
-    //Using for... of loop
-    for (let item of adventurer.inventory) {
-        console.log(item);
     }
+    // console.log(adventurer.companion.companion.belongings);
