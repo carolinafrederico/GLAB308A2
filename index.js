7fa5640ba0093c22742dba519329fbdb35a9b783
carolinafrederico <iam@carolinafrederico.com>
2025-03-23 23:42:31 -0400

    Part 2

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Part 1: Humble Beginnings
 // const adventurer = {
 //     name: "Robin",
 //     health: 10,
@@ -50,5 +51,26 @@ const adventurer = {
     }
 }
 adventurer.roll();      // Example Output: "Robin rolled a 14."
-adventurer.roll(2);      // Example Output: "Robin rolled a 17." (With a modifier of +2)
-adventurer.roll(-1);     // Example Output: "Robin rolled a 10." (With a modifier of -1)
+
+// Part 2: Class Fantasy
+class Character {
+    constructor (name) {
+      this.name = name;
+      this.health = 100;
+      this.inventory = [];
+    }
+    roll(mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a ${result}.`);
+      }
+    }
+
+// Now, re-create Robin using the Character class!
+    const robin = new Character("Robin");
+    robin.inventory = ["sword", "potion", "artifact"];
+    robin.companion = new Character("Leo");
+    robin.companion.type = "Cat";
+    robin.companion.companion = new Character("Frank");
+    robin.companion.companion.type = "Flea";
+    robin.companion.companion.inventory = ["small hat", "sunglasses"];
+    
