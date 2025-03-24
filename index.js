e1efc140058f64a1f605ed9e82002bc93352e3fb
carolinafrederico <iam@carolinafrederico.com>
2025-03-23 22:59:05 -0400

    Exercise 1

---
+++ index.js
@@ -0,0 +1,9 @@
+const adventurer = {
+    name: "Robin",
+    health: 10,
+    inventory: ["sword", "potion", "artifact"]
+    }
+    //Using for... of loop
+    for (let item of adventurer.inventory) {
+        console.log(item);
+    }
